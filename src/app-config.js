54fa1789ebc928c197043dbd246b13ba32e5d39f
sangwoo <bakch94@naver.com>
2022-04-25 13:04:42 +0900

    commit

--- src/app-config.js
+++ src/app-config.js
@@ -6,6 +6,7 @@ const hostname = window && window.location && window.location.hostname;
 //     // backendHost = "http://localhost:8080";
 // // } else {
     backendHost = "http://prod-todo-backend2.ap-northeast-2.elasticbeanstalk.com";
+    console.log("backend connection: " + backendHost);
 // }
 
 export const API_BASE_URL = `${backendHost}`;
